d8e6ace4e4535a3efbbf7d2c36d87196a5d8d3cc
tingjunman <tm2838@nyu.edu>
2021-10-18 21:36:57 -0500

    add sale_price

--- db/index.js
+++ db/index.js
@@ -5,6 +5,8 @@ const sampleProduct = require('./sampleProduct.json');
 mongoose.connect('mongodb://localhost:27017/test');
 
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); //eslint-disable-line
+db.once('open', () => console.log('db connected!')); //eslint-disable-line
 
 const { Schema } = mongoose;
 const productSchema = new Schema({
@@ -25,6 +27,7 @@ const productSchema = new Schema({
       style_id: Number,
       name: String,
       original_price: String,
+      sale_price: String || null,
       default: Boolean,
       photos: [
         {
@@ -46,7 +49,7 @@ const productSchema = new Schema({
 const Product = mongoose.model('Product', productSchema);
 
 Product.createCollection().then(() => {
-  console.log('collection created');
+  console.log('collection created'); //eslint-disable-line
 });
 
 const save = (products) => Product.create(products);
