7cd57244e6ea3bab2ba1e69a64c8e2ca0331a15c
tingjunman <tm2838@nyu.edu>
2021-10-29 16:06:36 -0500

    modify a little

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const { productSchema, styleSchema, skuSchema } = require('../db/schemas');
 
 const sampleProduct = require('../db/sampleProduct.json');
@@ -8,75 +9,62 @@ const sampleSkus = require('../db/sampleSkus.json');
 
 const { getProduct, getStyles } = require('../db/utils');
 
-const url = 'mongodb://127.0.0.1:27017';
+const url = 'mongodb://127.0.0.1:27017/test';
 
 describe('database utils', () => {
-  beforeAll((done) => {
-    mongoose.createConnection(`${url}/JestDB`,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => done());
-  });
-
-  afterAll((done) => {
-    mongoose.connection.close(() => done());
-  });
-
   // unit tests for database methods
 
   describe('getProduct', () => {
-    it('should get the requested product based on a valid product id', async () => {
-      const testProduct = mongoose.model('testProduct', productSchema);
-      const mockproduct = await testProduct.create(sampleProduct);
-      const product = await testProduct.findOne({ productId: 12345 });
+    xit('should get the requested product based on a valid product id', async () => {
+      const connection = await MongoClient.connect(url);
+      getProduct(1)
+        .then((response) => {
+          expect(typeof response).toBe('object');
 
-      expect(product.name).toBe('test product name');
-      expect(product.slogan).toBe('this is a fake slogan');
+          expect(response.id).toBe(1);
+          expect(response.name).toBe('Camo Onesie');
+        })
+        .then(() => {
+          connection.close();
+        });
     });
 
-    it('should return null if the provided product id is invalid', async () => {
-      const testProduct2 = mongoose.model('testProduct2', productSchema);
-      const product = await testProduct2.findOne({ productId: 12345 });
-
-      expect(product).toBe(null);
+    xit('should return null if the provided product id is invalid', async () => {
+      const connection = await MongoClient.connect(url);
+      getProduct(0)
+        .then((response) => {
+          expect(response).toBe(null);
+        })
+        .then(() => {
+          connection.close();
+        });
     });
   });
 
   describe('getStyles', () => {
     xit('should get the requested styles based on product id', async () => {
-      getStyles(12345)
+      const connection = await MongoClient.connect(url);
+      getStyles(1)
         .then((styles) => {
-          expect(styles[0]['default?']).toBe(true);
-          expect(styles[0].name).toBe('style1');
-          expect(styles[0].skus).toEqual({
-            4: {
-              size: 'L',
-              quantity: 10,
-            },
-            5: {
-              size: 'S',
-              quantity: 6,
-            },
-          });
-
-          expect(styles[1]['default?']).toBe(false);
-          expect(styles[1].name).toBe('style2');
-          expect(styles[1].skus).toEqual({
-            2: {
-              size: '7',
-              quantity: 14,
-            },
-            1: {
-              size: '8.5',
-              quantity: 7,
-            },
-          });
+          expect(styles[0]['default?']).toBe(false);
+          expect(styles[0].name).toBe('Desert Brown & Tan');
+          expect(styles[0].style_id).toBe(2);
+          expect(styles[0].photos).toBeTruthy();
+          expect(styles[0].skus).toBeTruthy();
+        })
+        .then(() => {
+          connection.close();
         });
     });
 
     xit('should return an empty array if the provided product id is invalid', async () => {
-      getStyles(1)
+      const connection = await MongoClient.connect(url);
+      getStyles(0)
         .then((styles) => {
           expect(styles).toEqual([]);
+        })
+        .then(() => {
+          connection.close();
         });
     });
   });
--- tests/integration.test.js
+++ tests/integration.test.js
@@ -3,12 +3,30 @@ const app = require('../server/server');
 
 describe('server routes', () => {
   it('GET /product/:productId', async () => {
-    supertest(app).get('http://localhost:3000/product/12345')
-      .expect(200);
+    supertest(app).get('http://localhost:3000/product/1')
+      .expect(200)
+      .then((response) => {
+        expect(typeof response).toBe('object');
+
+        expect(response.id).toBe(1);
+        expect(response.name).toBe('Camo Onesi');
+        expect(response.features).toHaveLength(2);
+      });
   });
 
   it('GET /product/:productId/styles', async () => {
-    supertest(app).get('http://localhost:3000/product/12345/styles')
-      .expect(200);
+    supertest(app).get('http://localhost:3000/product/1/styles')
+      .expect(200)
+      .then((response) => {
+        expect(Array.isArray(response)).toBe(true);
+
+        expect(response).toHaveLength(6);
+        expect(response[0].style_id).toBe(2);
+        expect(response[0].name).toBe('Desert Brown & Tan');
+        expect(response[0]['default?']).toBe(false);
+        expect(response[0].photos).toHaveLength(6);
+
+        expect(response[0].skus).toBeTruthy();
+      });
   });
 });
