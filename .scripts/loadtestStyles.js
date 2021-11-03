5a7ffc4ebea1917469f8502228c6f47a50f1ef5b
tingjunman <tm2838@nyu.edu>
2021-11-03 16:05:25 -0500

    more setup

--- .scripts/loadtestStyles.js
+++ .scripts/loadtestStyles.js
@@ -4,11 +4,12 @@ import { check, sleep } from 'k6';
 
 export default function () {
   const rnd = Math.floor(Math.random() * 100000) + 900000;
-  const response = http.get(`http://localhost:5000/product/${rnd}`);
+  const response = http.get(`http://localhost:5000/product/${rnd}/styles`);
   check(response, {
       "is status 200": (r) => r.status === 200,
       "is id the same we asked": (r) => {
-          const id = Number(r.json("id"))
+          console.log(r.json("productId"));
+          const id = Number(r.json("productId"))
           return id === rnd
       }
   })
