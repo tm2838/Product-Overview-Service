e99c85c0a397a8c2cb472fed53560828ecc7b564
Ubuntu <ubuntu@ip-172-31-20-245.ec2.internal>
2021-11-17 02:13:23 +0000

    optimization with redis

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
+const redis = require('redis');
+
+const redisPort = 6379;
+const client = redis.createClient({host: 'redis', port: redisPort});
+client.on('error', (err) => {
+  console.log(err); //eslint-disable-line
+});
+
 const { getProduct, getStyles } = require('../db/utils');
 
 const app = require('./server');
+
+app.get('/loaderio*', async (req, res) => {
+  res.status(200).send('loaderio-3ab2e7ca8cc7c87863235552afb40f7a');
+});
 
 app.get('/product/:productId', async (req, res) => {
   console.log('Got a request!'); //eslint-disable-line
@@ -8,12 +20,20 @@ app.get('/product/:productId', async (req, res) => {
   // get product data from database
   let product;
   try {
-    product = await getProduct(productId);
+    client.hget('products', productId, async (err, cachedProduct) => {
+      if (err) throw err;
+
+      if (cachedProduct) {
+        res.status(200).send(JSON.parse(cachedProduct));
+      } else {
+        product = await getProduct(productId);
+        client.hset('products', productId, JSON.stringify(product));
+        res.status(200).send(product);
+      }
+    });
   } catch (error) {
     res.status(500).send(error);
   }
-
-  res.status(200).send(product);
 });
 
 app.get('/product/:productId/styles', async (req, res) => {
@@ -21,12 +41,20 @@ app.get('/product/:productId/styles', async (req, res) => {
   // get style data from database
   let styles;
   try {
-    styles = await getStyles(productId);
+    client.hget('styles', `${productId}_styles`, async (err, cachedStyles) => {
+      if (err) throw err;
+
+      if (cachedStyles) {
+        res.status(200).send(JSON.parse(cachedStyles));
+      } else {
+        styles = await getStyles(productId);
+        client.hset('styles',`${productId}_styles`, JSON.stringify(styles));
+        res.status(200).send({ productId, results: styles });
+      }
+    });
   } catch (error) {
     res.status(500).send(error);
   }
-
-  res.status(200).send({ productId, results: styles });
 });
 
 app.listen(5000, () => {
